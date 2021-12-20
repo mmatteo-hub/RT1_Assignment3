5b0301eeb9a2475e670e56a5b214dc6bd688a61e
root <S46362126@studenti.unige.it>
2021-12-20 13:38:12 +0000

    modified service.cpp

--- final_assignment/src/service.cpp
+++ final_assignment/src/service.cpp
@@ -1,32 +1,26 @@
 #include "ros/ros.h"
 #include "std_srvs/Empty.h"
-#include "second_assignment/Service.h"
-
-// defining a variable to 
-std_srvs::Empty reset;
-
-// variable to pass the velocity change
-float change_term = 0;
+#include "final_assignment/Service.h"
 
 // switch to choose what to do given a certain input
-bool setVelocityFnc (second_assignment::Service::Request &req, second_assignment::Service::Response &res)
+bool setDriveMod (final_assignment::Service::Request &req, final_assignment::Service::Response &res)
 {
 	switch(req.input)
 	{
 		// publish a position (x,y)
-		case 1:
+		case '1':
 			break;
 		
 		// drive the robot with the move_base topic
-		case 2:
+		case '2':
 			break;
 			
 		// drive the robot with the teleop_twist_kwyboard
-		case 3:
+		case '3':
 			break;
 			
 		// kill all nodes
-		case 0:
+		case '0':
 			ros::shutdown();
 			break;
 			
@@ -47,7 +41,7 @@ int main(int argc, char ** argv)
 	// defininf a node handle
 	ros::NodeHandle nh;
 	// advertise the service and call the function
-	ros::ServiceServer service = nh.advertiseService("/service", setVelocityFnc);
+	ros::ServiceServer service = nh.advertiseService("/service", setDriveMod);
 	
 	// spin the program
 	ros::spin();
